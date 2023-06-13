43aadd36697e37df252290c6e7e999a71103dd26
vaibhav <vaibhav@viralmoment.com>
2023-06-13 15:50:21 +0530

    add models

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class SecurityAttendance(models.Model):
+    security_guard = models.ForeignKey(User, related_name='security_guard', on_delete=models.CASCADE)
+    check_in_time = models.TimeField()
+    check_out_time = models.TimeField()
+    created_at = models.DateField(auto_now_add=True)
+
+
+class AttendanceHistory(models.Model):
+    attendance_id = models.ForeignKey(SecurityAttendance, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='uploads/')
+    upload_time = models.TimeField(auto_now_add=True)
+    upload_date = models.DateField(auto_now_add=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
